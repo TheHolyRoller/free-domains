9bb362d307f58536ac09bac88790fc240d35f28b
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-10-20 22:14:25 +0200

    Create light2albania.is-an.app.js
    A website for my parents charity 
    
    Type 	Name 	Value
    TXT 	_vercel 	vc-domain-verify=website-1e76426za-daniel-wakeley-s-projects.vercel.app
    
    
    Link to website: 
    
    https://website-rho-ashy.vercel.app/

---
+++ light2albania.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A website for my parents non-profit charity',
+  domain: 'is-an.app',
+  subdomain: 'light2albania',
+  owner: {
+    email: 'danielwakeley7@gmail.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: false
+});
