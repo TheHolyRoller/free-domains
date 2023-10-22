14ec017e2dd42b7ce3cde31f04da173716bface4
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-10-22 20:16:22 +0200

    Create light2all.is-an.app.js

---
+++ domains/light2all.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Website for charity',
+  domain: 'is-an.app',
+  subdomain: 'ligth2all',
+  owner: {
+    repo: 'https://github.com/TheHolyRoller',
+    email: 'danielwakeley7@gmail.com',
+  },
+  record: {
+    CNAME: 'https://website-rho-ashy.vercel.app/',
+  },
+  proxy: false,
+})
